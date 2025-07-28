6f66d2652670f1deb0556ab359260344abb64837
43203449870 <salomao.silva@serpro.gov.br>
2025-07-27 22:01:54 -0300

    Ajustando URLs

--- loja_de_veiculos/urls.py
+++ loja_de_veiculos/urls.py
@@ -5,16 +5,16 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Minha API",
-      default_version='v1',
-      description="Documentação Swagger da API",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Minha API",
+        default_version='v1',
+        description="Documentação Swagger da API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-    path('', lambda request: redirect('swagger/', permanent=False)),  # redireciona para /swagger/
+    path('', lambda request: redirect('swagger/', permanent=False)),
 
     path('veiculos/', include('veiculos.urls')),
     path('pessoas/', include('pessoas.urls')),
--- veiculos/urls.py
+++ veiculos/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [    
     path('', VeiculoCreateView.as_view(), name='cadastrar_veiculo'),
     path('<int:pk>/', VeiculoUpdateView.as_view(), name='editar_veiculo'),
+    path('<int:pk>/excluir/', VeiculoDeleteView.as_view(), name='veiculo-excluir'),
     path('disponiveis/', VeiculosAVendaListView.as_view(), name='veiculos_a_venda'),
     path('vendidos/', VeiculosVendidosListView.as_view(), name='veiculos_vendidos'),
     path('comprar/', ComprarVeiculoView.as_view(), name='comprar_veiculo'),
